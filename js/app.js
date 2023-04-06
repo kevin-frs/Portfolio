66c3452dc35f15e642d8ce8b764c17e2c0a0f65d
kevin-frs <kevloux@gmail.com>
2023-04-06 09:58:26 +0200

    Init

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load(
+  "particles-js",
+  "../resources/jsons/particles.json",
+  function () {
+    console.log("callback - particles.js config loaded");
+  }
+);
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
