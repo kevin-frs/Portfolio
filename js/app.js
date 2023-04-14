8ca1b05fe62b196751f308ff91ada42ec27501d2
kevin-frs <kevloux@gmail.com>
2023-04-14 22:05:00 +0200

    Version rendu (+responsive)

--- js/app.js
+++ js/app.js
@@ -7,14 +7,43 @@ particlesJS.load(
   }
 );
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
+/****** Menu burder for mobile ******/
+window.onload = () => {
+  var sidenav = document.getElementById("mySidenav");
+  var openBtn = document.getElementById("openBtn");
+  var closeBtn = document.getElementById("closeBtn");
+
+  openBtn.onclick = openNav;
+  closeBtn.onclick = closeNav;
+
+  /* Set the width of the side navigation to 250px */
+  function openNav() {
+    sidenav.classList.add("active");
   }
-  prevScrollpos = currentScrollPos;
+
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    sidenav.classList.remove("active");
+  }
 };
+
+/* When the user scrolls down, change the navbar. */
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var navbar = document.getElementById("navbar");
+  var inside_nav = document.querySelector("#navbar .container");
+  if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
+    //When scrolled
+
+    navbar.style.background = "#000c";
+    inside_nav.style.height = "50px";
+  } else {
+    //Base
+
+    navbar.style.background = "#000F";
+    inside_nav.style.height = "75px";
+  }
+}
